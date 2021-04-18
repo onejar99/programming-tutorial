3a56d871cbd4c18be62b0dbad43181afc014374e
OneJar <onejar99@gmail.com>
2021-04-18 13:05:52 +0800

    [Python tutorials] add Practice 1: win a lottery

---
+++ tutorial-paython/practices/practice_lottery/practice1_lottery.py
@@ -0,0 +1,7 @@
+bonus = 1000000
+tax = bonus * 0.4
+net = bonus - tax
+print("Congratulations! You win a lottery!")
+print("Bonus: $" + str(bonus))
+print("Tax: $" + str(int(tax)))
+print("Net: $" + str(int(net)))
